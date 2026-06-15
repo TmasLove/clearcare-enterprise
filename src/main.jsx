64f1f79ddfd015a83e0458eedeecf913b5147638
Tommy <troldan92@gmail.com>
2026-06-15 13:56:05 -0400

    chore: scaffold Vite + React enterprise site

---
+++ src/main.jsx
@@ -0,0 +1,5 @@
+// src/main.jsx
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+function App() { return <div>Clear Care Enterprise</div>; }
+createRoot(document.getElementById('root')).render(<React.StrictMode><App /></React.StrictMode>);
